2db7787870f79124840642a96e1f82c5840e04b8
shobhitm1 <shobhitm182@gmail.com>
2020-06-15 19:04:44 +0530

    About Lambda Concept in Java 8

--- src/Java8Concepts/lambdaConcept.java
+++ src/Java8Concepts/lambdaConcept.java
@@ -3,8 +3,15 @@ package Java8Concepts;
 public class lambdaConcept {
 
 	public static void main(String[] args) {
+		//-> lambda expresssion
+		//SAM :-Lambda expression allows only single abstract method.It is known as functional interface. 
+		//Enable to treat functionality as a method argument, or code as data.
+		//Lambda expressions basically express instances of functional interfaces 
 		
-
+		webpage w1 = (value)->System.out.println("hi"+" "+value);
+		//w1.header("google");
+		 //webpage w1 =(String value,int key)->System.out.println(value+" "+key);
+		// w1.header("Shobhit",22);
+		 w1.header("Shobhit");
 	}
-
 }
---
+++ src/Java8Concepts/webpage.java
@@ -0,0 +1,7 @@
+package Java8Concepts;
+
+public interface webpage {
+	
+	 void header(String value);	
+	 //  void header(String value,int age); 
+}
